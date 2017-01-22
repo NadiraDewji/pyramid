457e8ac2574db120f9134f5e7bc5c9cafc73c706
Nadira Azim Dewji <nad372@nyu.edu>
2017-01-22 13:13:06 -0500

    added pyramid

---
+++ Pyramid.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Pyramid {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the height of your pyramid △");
+	}
+}
